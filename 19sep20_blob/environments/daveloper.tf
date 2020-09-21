6c4f5513a78fc45fb38241b8b4de135420bd60f6
drhbigdave <drhbigdave@users.noreply.github.com>
2020-09-21 13:10:10 -0400

    renamed rg

--- 19sep20_blob/environments/daveloper.tf
+++ 19sep20_blob/environments/daveloper.tf
@@ -4,7 +4,7 @@ module "python_sdk_exercises" {
   permanent_infra_rg_name        = "permanent_infra"
   davids_home_ip_secret_name     = "davids-home-ip"
   shanikas_home_ip_secret_name   = "shanikas-home-ip"
-  rg1_name                       = "19sep20blob"
+  rg1_name                       = "az_sdk_exercise"
   git_repo_tag = "https://github.com/drhbigdave/az_sdk_exercise"
   rg1_location                   = "East US"
   sa1_name                       = "pythonexercies"
